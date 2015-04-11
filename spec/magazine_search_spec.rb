37d6bcf3a93f75e7d7c19c22c19d5990645b016f
Denise <deniseschrack@gmail.com>
2015-04-10 23:38:27 -0700

    Removed the spec helper garbage

--- spec/magazine_search_spec.rb
+++ spec/magazine_search_spec.rb
@@ -1,11 +1,3 @@
-require 'capybara'
-require 'capybara/rspec'
-require 'selenium-webdriver'
-require 'pry'
-
-Capybara.default_driver = :selenium
-Capybara.app_host = 'http://magnet-production-1426654625.elasticbeanstalk.com'
-
 describe "magazine search", :type => :feature do
   it "finds popular science in 93101" do
     visit '/'
